26569b56b05e9cbee2a344b0fa6e91718c5ce8aa
Rudresh Pandey <126448002+RudreshPandey203@users.noreply.github.com>
2024-03-28 00:06:38 +0530

    Update page.jsx

--- app/teacher/[teacherid]/calendar/page.jsx
+++ app/teacher/[teacherid]/calendar/page.jsx
@@ -2,15 +2,6 @@
 // import React, {ReactNode, SyntheticEvent} from 'react';
 // import ApiCalendar from 'react-google-calendar-api';
 
-// const config = {
-//   "clientId": process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-//   "apiKey": process.env.NEXT_PUBLIC_API_KEY,
-//   "scope": "https://www.googleapis.com/auth/calendar",
-//   "discoveryDocs": [
-//     "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"
-//   ]
-// }
-
 // const apiCalendar = new ApiCalendar(config)
 // console.log("config", config)
 
